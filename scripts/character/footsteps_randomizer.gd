9c8b3dc12caf2524b94d2add7228137489f5831c
Wagner Scholl Lemos <josephblt@gmail.com>
2025-04-28 01:55:37 -0300

    Change from error to warning in case of footstep not found.

--- scripts/character/footsteps_randomizer.gd
+++ scripts/character/footsteps_randomizer.gd
@@ -21,23 +21,23 @@ func play_footstep() -> void:
 
 func _fetch_random_footstep() -> AudioStream:
 	if !terrain_tiles:
-		printerr("FootstepsRandomizer: No terrain tiles assigned.")
+		push_warning("FootstepsRandomizer: No terrain tiles assigned.")
 		return null
 	
 	var tile_coords: Vector2i = terrain_tiles.local_to_map(terrain_tiles.to_local(character.position))
 	var tile_data: TileData = terrain_tiles.get_cell_tile_data(tile_coords)
 	if !tile_data:
-		printerr("FootstepsRandomizer: No tile data found at coordinates: ", tile_coords)
+		push_warning("FootstepsRandomizer: No tile data found at coordinates: ", tile_coords)
 		return null
 	
 	var footsteps_resource: FootstepsResource = tile_data.get_custom_data("Footsteps")
 	if !footsteps_resource:
-		printerr("FootstepsRandomizer: No footsteps resource found or empty.")
+		push_warning("FootstepsRandomizer: No footsteps resource found or empty.")
 		return null
 	
 	var footsteps: Array[AudioStream] = footsteps_resource.footsteps_sfx
 	if !footsteps or footsteps.size() == 0:
-		printerr("FootstepsRandomizer: No footsteps found in the resource.")
+		push_warning("FootstepsRandomizer: No footsteps found in the resource.")
 		return null
 	
 	return footsteps.pick_random()
